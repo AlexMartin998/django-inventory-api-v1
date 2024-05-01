bd94d75834437e496527899606f56368bf807952
Adrian <desarrollador2@fibramax.ec>
2024-04-30 22:53:11 -0500

    ADD: add routes for swagger un general urls.py | [SAGGER]

--- backend/urls.py
+++ backend/urls.py
@@ -18,9 +18,42 @@ from django.contrib import admin
 from django.urls import path, include
 
 
+
+# ### Swagger
+import os
+from rest_framework import permissions
+from django.urls import path, re_path
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="My API",
+        default_version='v1',
+        description="API description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@myapi.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+    url=os.environ.get('API_URL')
+)
+
 urlpatterns = [
+    # ### Swagger
+    re_path(r'^api/v1/swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('api/v1/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('api/v1/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
+
+    # ### Admin
     path('admin/', admin.site.urls),
     
+    
+    # ### API
     # ## set v1 as version: no se puede colocar 1 prefix generico como en spring o nestjs
     path('api/v1/users/', include('users.urls')),
     path('api/v1/categories/', include('products.urls.category_urls')),
