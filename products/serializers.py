9e90b49f9662c35e21ab58ee04da9f0286918d22
Adrian <alextravc.342@gmail.com>
2024-05-02 00:07:47 -0500

    ADD: Subcategory serializers (model serializer + filters serializer)

--- products/serializers.py
+++ products/serializers.py
@@ -1,9 +1,10 @@
 from rest_framework import serializers
 
 from backend.serializers import FiltersBaseSerializer
-from products.models import Category
+from products.models import Category, SubCategory
 
 
+# ### Category =================================
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
@@ -14,5 +15,18 @@ class CategorySerializer(serializers.ModelSerializer):
 class CategoryFilterSerializer(FiltersBaseSerializer):
     class Meta:
         model = Category
+        fields = '__all__'
+
+
+# ### Subcategory =================================
+class SubcategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SubCategory
+        fields = '__all__'
+
+
+class SubcategoryFilterSerializer(FiltersBaseSerializer):
+    class Meta:
+        model = SubCategory
         fields = '__all__'
 
