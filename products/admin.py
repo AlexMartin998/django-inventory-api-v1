4e5aacd599ac8cadb96813aa0e9da40569295ddb
Adrian <alextravc.342@gmail.com>
2024-05-03 21:13:53 -0500

    ADD: models included in /admin django panel

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from products.models.category_model import Category
+from products.models.subcategory_model import SubCategory
+from products.models.product_model import Product
+from products.models.review_model import Reviews
+
+
+# ### Register your MODELS here (/admin) de Django
+admin.site.register(Category)
+admin.site.register(Reviews)
+admin.site.register(Product)
+admin.site.register(SubCategory)
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User
+
+# ### Register your MODELS here (/admin) de Django
+admin.site.register(User)
+
