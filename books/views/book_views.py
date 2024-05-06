2da264bef5ef03788153d6103d04f47ed2d98319
Adrian <alextravc.342@gmail.com>
2024-05-05 23:18:07 -0500

    ADD: BookView + BookDetailView

--- books/views/book_views.py
+++ books/views/book_views.py
@@ -1,20 +1,20 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+# authentication
+from rest_framework.permissions import IsAuthenticated
+
 # ## docs openapi
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
 
 
-from backend.shared.views.general_view import GeneralAPIView, GeneralDetailAPIView
 from backend.shared.constants.constants import page_size_openapi, page_openapi
 from backend.shared.serializers.serializers import (
     BadRequestSerializerDoc,
     NotFoundSerializer,
 )
 
-from books.filters.book_filters import BookFilter
-from books.models.book_model import Book
 from books.serializers.book_serializers import (
     BookSerializer,
     BookQueryDocWrapperSerializer,
@@ -23,7 +23,11 @@ from books.serializers.book_serializers import (
     BookOptDocSerializer,
 )
 
-from backend.shared.utils.pagination import CustomPagination
+# ### Services ==============================
+from rest_framework import status
+from backend.shared.helpers.handle_rest_exception_helper import (
+    handle_rest_exception_helper,
+)
 from backend.shared.constants.constants import (
     PAGINATION_DEFAULT_PAGE_NUMBER,
     PAGINATION_DEFAULT_PAGE_SIZE,
@@ -35,11 +39,7 @@ from books.services.book_service import BookService
 
 
 class BookView(APIView):
-    model = Book
-    filter = BookFilter
-
-    serializer = BookSerializer  # model serializer
-    serializer2 = BookResponseSerializer  # response
+    permission_classes = [IsAuthenticated]
 
     # constructor
     def __init__(self):
@@ -65,17 +65,37 @@ class BookView(APIView):
         )
 
         # Call the get_all method
-        books_data = self.book_service.get_all(filter_params, page_number, page_size)
+        books_data = self.book_service.find_all(filter_params, page_number, page_size)
 
         # Return the paginated response
         return Response(books_data)
 
+    @swagger_auto_schema(
+        operation_description="Create Book",
+        request_body=BookSerializer,
+        responses={
+            201: openapi.Response("OK", BookOptDocSerializer),
+            400: openapi.Response("Bad Request", BadRequestSerializerDoc),
+        },
+    )
+    def post(self, request):
+        try:
+            serialized_book = self.book_service.create(request.data)
+            return Response(serialized_book, status=status.HTTP_201_CREATED)
+        except Exception as e:
+            return self.handle_exception(e)
 
-class BookDetailView(GeneralDetailAPIView):
-    model = Book
+    def handle_exception(self, exc):
+        return handle_rest_exception_helper(exc)
 
-    serializer = BookSerializer
-    serializer2 = BookResponseSerializer
+
+class BookDetailView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    # constructor
+    def __init__(self):
+        self.book_repository = BookRepository()
+        self.book_service = BookService(self.book_repository)
 
     @swagger_auto_schema(
         operation_description="Get Book by ID",
@@ -85,7 +105,11 @@ class BookDetailView(GeneralDetailAPIView):
         },
     )
     def get(self, request, pk):
-        return super().get(request, pk)
+        try:
+            serialized_book = self.book_service.find_one(pk)
+            return Response(serialized_book)
+        except Exception as e:
+            return self.handle_exception(e)
 
     @swagger_auto_schema(
         operation_description="Update Book",
@@ -97,7 +121,11 @@ class BookDetailView(GeneralDetailAPIView):
         },
     )
     def patch(self, request, pk):
-        return super().patch(request, pk)
+        try:
+            serialized_book = self.book_service.update(pk, request.data)
+            return Response(serialized_book)
+        except Exception as e:
+            return self.handle_exception(e)
 
     @swagger_auto_schema(
         operation_description="Delete Book",
@@ -107,4 +135,11 @@ class BookDetailView(GeneralDetailAPIView):
         },
     )
     def delete(self, request, pk):
-        return super().delete(request, pk)
+        try:
+            self.book_service.delete(pk)
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Exception as e:
+            return self.handle_exception(e)
+
+    def handle_exception(self, exc):
+        return handle_rest_exception_helper(exc)
