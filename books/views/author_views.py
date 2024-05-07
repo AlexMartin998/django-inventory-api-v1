cd5aba8e326377ced8338cbcea9ff272ecb973ef
Adrian <alextravc.342@gmail.com>
2024-05-06 23:08:34 -0500

    ADD: GeneralAPIViewService

--- books/views/author_views.py
+++ books/views/author_views.py
@@ -1,3 +1,104 @@
+# ## docs openapi
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+
+
+from backend.shared.constants.constants import page_size_openapi, page_openapi
+from backend.shared.serializers.serializers import (
+    BadRequestSerializerDoc,
+    NotFoundSerializer,
+)
+
+from books.repositories.author_repository import AuthorRepository
+from books.services.author_service import AuthorService
+from books.serializers.author_serializers import (
+    AuthorSerializer,
+    AuthorQueryDocWrapperSerializer,
+    AuthorResponseSerializer,
+    AuthorFilterSerializer,
+    AuthorOptDocSerializer,
+)
+
+from backend.shared.views.general_view_service import (
+    GeneralAPIViewService,
+    GeneralDetailAPIViewService,
+)
+
+
+class AuthorView(GeneralAPIViewService):
+
+    # constructor
+    def __init__(self):
+        author_repository = AuthorRepository()
+        author_service = AuthorService(author_repository)
+        super().__init__(author_service)
+
+    @swagger_auto_schema(
+        operation_description="Get All Authors",
+        responses={
+            200: openapi.Response("OK", AuthorQueryDocWrapperSerializer),
+        },
+        query_serializer=AuthorFilterSerializer,
+        manual_parameters=[page_size_openapi, page_openapi],
+    )
+    def get(self, request):
+        return super().get(request)
+
+    @swagger_auto_schema(
+        operation_description="Create Author",
+        request_body=AuthorSerializer,
+        responses={
+            201: openapi.Response("OK", AuthorOptDocSerializer),
+            400: openapi.Response("Bad Request", BadRequestSerializerDoc),
+        },
+    )
+    def post(self, request):
+        return super().post(request)
+
+
+class AuthorDetailView(GeneralDetailAPIViewService):
+
+    # constructor
+    def __init__(self):
+        author_repository = AuthorRepository()
+        author_service = AuthorService(author_repository)
+        super().__init__(author_service)
+
+    @swagger_auto_schema(
+        operation_description="Get Author by ID",
+        responses={
+            200: openapi.Response("OK", AuthorResponseSerializer),
+            404: openapi.Response("Not Found", NotFoundSerializer),
+        },
+    )
+    def get(self, request, pk):
+        return super().get(request, pk)
+
+    @swagger_auto_schema(
+        operation_description="Update Author",
+        request_body=AuthorSerializer,
+        responses={
+            200: openapi.Response("OK", AuthorOptDocSerializer),
+            404: openapi.Response("Not Found", NotFoundSerializer),
+            400: openapi.Response("Bad Request", BadRequestSerializerDoc),
+        },
+    )
+    def patch(self, request, pk):
+        return super().patch(request, pk)
+
+    @swagger_auto_schema(
+        operation_description="Delete Author",
+        responses={
+            204: openapi.Response("Ok - No Content"),
+            404: openapi.Response("Not Found", NotFoundSerializer),
+        },
+    )
+    def delete(self, request, pk):
+        return super().delete(request, pk)
+
+
+"""  Views with no inheritance
+
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -125,3 +226,5 @@ class AuthorDetailView(APIView):
 
     def handle_exception(self, exc):
         return handle_rest_exception_helper(exc)
+
+"""
